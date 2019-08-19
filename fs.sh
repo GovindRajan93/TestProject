9df14d8d745e4b2f6931f266a883bc334a37ec1b
GovindRajan93 <54279821+GovindRajan93@users.noreply.github.com>
2019-08-19 22:51:11 +0530

    Update fs.sh

--- fs.sh
+++ fs.sh
@@ -1,11 +1,11 @@
-hostname=`hostname` 2> /dev/null
-ip=`hostname -I` 2> /dev/null; echo "$(tput setaf 6)"; echo "$hostname:$ip"
-uptime=`uptime | cut -d ',' -f1,2 | cut -b 13-28` 2> /dev/null; echo "uptime:$uptime"
-load=`uptime | cut -d ',' -f4 | cut -d ':' -f2 | cut -d '.' -f1` 2> /dev/null
+hostname=`hostname`
+ip=`hostname -I`; echo "$(tput setaf 6)"; echo "$hostname:$ip"
+uptime=`uptime | cut -d ',' -f1,2 | cut -b 13-28`; echo "uptime:$uptime"
+load=`uptime | cut -d ',' -f4 | cut -d ':' -f2 | cut -d '.' -f1`
 thresh=5
 if [ $load -lt $thresh ]
 then
-echo "$(tput setaf 2)Load is OK"; echo "Current Load:" $load
+echo "Load is OK"; echo "Current Load:" $load
 else
-echo "$(tput setaf 1)Load is HIGH"; echo "Current Load:" $load
+echo "Load is HIGH"; echo "Current Load:" $load
 fi
