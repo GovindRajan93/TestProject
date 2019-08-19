a4e826f67287e8a75bc8e8129107f4dea34daa3d
GovindRajan93 <54279821+GovindRajan93@users.noreply.github.com>
2019-08-19 22:55:28 +0530

    Update fs.sh

--- fs.sh
+++ fs.sh
@@ -1,11 +1,4 @@
 hostname=`hostname`
 ip=`hostname -I`; echo "$hostname:$ip"
-uptime=`uptime | cut -d ',' -f1,2 | cut -b 13-28`; echo "uptime:$uptime"
-load=`uptime | cut -d ',' -f4 | cut -d ':' -f2 | cut -d '.' -f1`
-thresh=5
-if [ $load -lt $thresh ]
-then
-echo "Load is OK"; echo "Current Load:" $load
-else
-echo "Load is HIGH"; echo "Current Load:" $load
-fi
+uptime=`uptime`; echo "uptime:$uptime"
+echo " The script is tested"
